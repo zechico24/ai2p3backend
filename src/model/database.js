f81c98705c2f4eb85b251181e97336024e8cffa8
zechico24 <franciscosssssssssssss@gmail.com>
2022-07-19 23:46:41 +0100

    Update database.js

--- src/model/database.js
+++ src/model/database.js
@@ -1,10 +1,10 @@
 var Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-'trabalhoAI',
-'postgres',
-'1234',
+'ddt75b1hm23i8n',
+'cwyfwqmqcvgaim',
+'5bf2d5db3cfb332df5ea218d55825e82e29546a35a65276937363e2ca3948e58',
 {
-host: 'localhost',
+host: 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com',
 port: '5432',
 dialect: 'postgres'
 }
